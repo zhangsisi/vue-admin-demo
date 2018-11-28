45ed17af84d034c1ad0498820146289502a1bab6
zhangsisi <ssyy4411@163.com>
2018-11-28 20:13:47 +0800

    組件加element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <el-button>默认按钮</el-button>
+    <HelloWorld />
   </div>
 </template>
 
@@ -9,20 +9,11 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
   components: {
     HelloWorld
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
 </style>
